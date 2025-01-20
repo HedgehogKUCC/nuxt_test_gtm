442e087f426409185da306cea4a0595800bd597f
LUNGYEN\conrad.ku <conrad.ku@lyls.com.tw>
2025-01-20 13:46:04 +0800

    feat: 在首頁新增 GTM 事件推送按鈕

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,25 @@
 <template>
-  <Tutorial />
+  <div>
+    <Tutorial />
+    <el-button @click="triggerGTM">
+      GTM push event
+    </el-button>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  methods: {
+    triggerGTM () {
+      window.dataLayer = window.dataLayer || []
+
+      window.dataLayer.push({
+        event: 'click_generateObituary',
+        obituary_id: 9999,
+        erp_case_id: 1234567890
+      })
+    }
+  }
 }
 </script>
